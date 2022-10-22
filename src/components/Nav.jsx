8e22c17c533427b76517e5e427f966d99f3713a4
Jitender Kumar <ohyajk@gmail.com>
2022-10-22 08:30:42 +0530

    first

---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Nav = () => {
+    return (
+        <>
+            <div className='absolute flex justify-center w-full'>
+                <div className='absolute nav h-20 w-3/4 mt-4 rounded-full flex justify-between items-center px-10 text-center'>
+                    <img className=' h-14 object-cover' src="logo.png" alt="logo" />
+                    <div className='relative flex gap-8 links'>
+                        <a>HOME</a>
+                        <a>ABOUT</a>
+                        <a>SERVICES</a>
+                        <a>PROJECTS</a>
+                        <a>CONTACT</a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
