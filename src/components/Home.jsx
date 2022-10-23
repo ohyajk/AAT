12d0c43b62ecbbe6d68fc2e1fde1e0e29d9bfccf
Jitender Kumar <ohyajk@gmail.com>
2022-10-23 17:39:54 +0530

    resone

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,14 +4,17 @@ export const Home = () => {
     return (
         <>
             <div className='flex flex-col lg:flex-row justify-center items-center w-full gap-2 lg:gap-4 py-16 px-4 lg:px-28'>
-                <div className='w-1/2'>
+                <div className='lg:w-1/2'>
                     <img className=' object-scale-down' src="hero.png" alt="hero" />
                 </div>
-                <div className='w-1/2'>
+                <div className='lg:w-1/2'>
                     <h1 className='font-extrabold text-[48px] text-[#2d2fbb] text-center lg:text-left'>
                         We are here to make you digital.
                     </h1>
                 </div>
+            </div>
+            <div>
+                <img src="" alt="" />
             </div>
         </>
     )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export const Nav = () => {
     return (
         <>
-            <div className='flex justify-center w-full font-medium'>
+            {/* PC NAV */}
+            <div className='hidden lg:flex justify-center w-full font-medium'>
                 <div className='h-20 w-3/4 mt-2 rounded-full flex justify-between items-center px-10 text-center border-2 border-black/20 shadow-black/25 shadow-lg nav'>
                     <img className='h-14 object-cover' src="logo.png" alt="logo" />
                     <div className='flex gap-8 links'>
@@ -12,6 +13,15 @@ export const Nav = () => {
                         <a>SERVICES</a>
                         <a>PROJECTS</a>
                         <a>CONTACT</a>
+                    </div>
+                </div>
+            </div>
+            {/* MOBILE NAV */}
+            <div className='lg:hidden flex justify-center w-full font-medium'>
+                <div className='h-20 w-11/12 mt-2 rounded-full flex justify-between items-center px-10 text-center border-2 border-black/20 shadow-black/25 shadow-lg nav'>
+                    <img className='h-14 object-cover' src="logo.png" alt="logo" />
+                    <div>
+                        Hello
                     </div>
                 </div>
             </div>
