7f95f02fc9ba8abe8077ee29baecc1b8fad9f38e
Jitender Kumar <ohyajk@gmail.com>
2022-10-23 16:39:19 +0530

    second home edit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,15 +3,16 @@ import React from 'react'
 export const Home = () => {
     return (
         <>
-            <div className='absolute h-screen w-full flex items-end'>
-                <img className=' h-96 w-full top-0' src="layer.png" alt="layer" />
+            <div className='flex flex-col lg:flex-row justify-center items-center w-full gap-2 lg:gap-4 py-16 px-4 lg:px-28'>
+                <div className='w-1/2'>
+                    <img className=' object-scale-down' src="hero.png" alt="hero" />
+                </div>
+                <div className='w-1/2'>
+                    <h1 className='font-extrabold text-[48px] text-[#2d2fbb] text-center lg:text-left'>
+                        We are here to make you digital.
+                    </h1>
+                </div>
             </div>
-            <div>
-
-            </div>
-
-
-
         </>
     )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Nav = () => {
     return (
         <>
-            <div className='absolute flex justify-center w-full'>
+            <div className='flex justify-center w-full font-medium'>
                 <div className='h-20 w-3/4 mt-2 rounded-full flex justify-between items-center px-10 text-center border-2 border-black/20 shadow-black/25 shadow-lg nav'>
                     <img className='h-14 object-cover' src="logo.png" alt="logo" />
                     <div className='flex gap-8 links'>
