e7b950fbb3e3865e842f4b21382e2490f712397d
Jitender Kumar <ohyajk@gmail.com>
2022-10-29 18:41:01 +0530

    testimony

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 export const Home = () => {
     return (
@@ -180,11 +182,11 @@ export const Home = () => {
                 </div>
             </div>
             {/* Our Features */}
-            <div className='bg-[#f4f4f4]'>
+            <div className='bg-[#f4f4f4] pb-8'>
                 <h1 className='text-4xl text-center text-[#2d2fbb] font-bold pt-8'>
                     Our Features
                 </h1>
-                <p className='text-center pt-2 w-3/4 mx-auto'>
+                <p className='text-center pt-2 pb-4 w-3/4 mx-auto'>
                     Our features include the following:
                 </p>
                 {/* 6 divs */}
@@ -244,6 +246,84 @@ export const Home = () => {
                         </div>
                     </div>
                 </div>
+            </div>
+            {/* Services */}
+            <div className='bg-white flex flex-col justify-center items-center pb-8'>
+                <h1 className='text-4xl text-center text-[#2d2fbb] font-bold pt-8'>
+                    Our Services
+                </h1>
+                {/* 4 Services */}
+                <div className='grid grid-cols-1 lg:grid-cols-2 text-center gap-6 px-2 lg:px-16 py-8 w-[92%] lg:w-[60%]'>
+                    <div className='flex justify-start bg-[#ea4d2d] hover:bg-[#2d2fbb] rounded-l-full rounded-r-full '>
+                        <div className='bg-white p-2 rounded-full border-2 border-[#2d2fbb]'>
+                            <svg class="h-8 w-8" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="desktop" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="#2d2fbb" d="M528 0H48C21.5 0 0 21.5 0 48v320c0 26.5 21.5 48 48 48h192l-16 48h-72c-13.3 0-24 10.7-24 24s10.7 24 24 24h272c13.3 0 24-10.7 24-24s-10.7-24-24-24h-72l-16-48h192c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48zm-16 352H64V64h448v288z"></path></svg>
+                        </div>
+                        <div className='flex justify-center text-center items-center text-white w-full'>
+                            <h1>Web Designing & Development</h1>
+                        </div>
+                    </div>
+                    <div className='flex justify-start bg-[#ea4d2d] hover:bg-[#2d2fbb] rounded-l-full rounded-r-full '>
+                        <div className='bg-white p-2 rounded-full border-2 border-[#2d2fbb]'>
+                            <svg class="h-8 w-8" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="comment-dots" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="#2d2fbb" d="M256 32C114.6 32 0 125.1 0 240c0 49.6 21.4 95 57 130.7C44.5 421.1 2.7 466 2.2 466.5c-2.2 2.3-2.8 5.7-1.5 8.7S4.8 480 8 480c66.3 0 116-31.8 140.6-51.4 32.7 12.3 69 19.4 107.4 19.4 141.4 0 256-93.1 256-208S397.4 32 256 32zM128 272c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm128 0c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm128 0c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32z"></path></svg>
+                        </div>
+                        <div className='flex justify-center text-center items-center text-white w-full'>
+                            <h1>Bulk SMS Services</h1>
+                        </div>
+                    </div>
+                    <div className='flex justify-start bg-[#ea4d2d] hover:bg-[#2d2fbb] rounded-l-full rounded-r-full '>
+                        <div className='bg-white p-2 rounded-full border-2 border-[#2d2fbb]'>
+                            <svg class="h-8 w-8" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="mobile-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="#2d2fbb" d="M272 0H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h224c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48zM160 480c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm112-108c0 6.6-5.4 12-12 12H60c-6.6 0-12-5.4-12-12V60c0-6.6 5.4-12 12-12h200c6.6 0 12 5.4 12 12v312z"></path></svg>
+                        </div>
+                        <div className='flex justify-center text-center items-center text-white w-full'>
+                            <h1>Android & iOS Apps Development</h1>
+                        </div>
+                    </div>
+                    <div className='flex justify-start bg-[#ea4d2d] hover:bg-[#2d2fbb] rounded-l-full rounded-r-full w-full'>
+                        <div className='bg-white p-2 rounded-full border-2 border-[#2d2fbb]'>
+                            <svg class="h-8 w-8" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bullhorn" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="#2d2fbb" d="M576 240c0-23.63-12.95-44.04-32-55.12V32.01C544 23.26 537.02 0 512 0c-7.12 0-14.19 2.38-19.98 7.02l-85.03 68.03C364.28 109.19 310.66 128 256 128H64c-35.35 0-64 28.65-64 64v96c0 35.35 28.65 64 64 64h33.7c-1.39 10.48-2.18 21.14-2.18 32 0 39.77 9.26 77.35 25.56 110.94 5.19 10.69 16.52 17.06 28.4 17.06h74.28c26.05 0 41.69-29.84 25.9-50.56-16.4-21.52-26.15-48.36-26.15-77.44 0-11.11 1.62-21.79 4.41-32H256c54.66 0 108.28 18.81 150.98 52.95l85.03 68.03a32.023 32.023 0 0 0 19.98 7.02c24.92 0 32-22.78 32-32V295.13C563.05 284.04 576 263.63 576 240zm-96 141.42l-33.05-26.44C392.95 311.78 325.12 288 256 288v-96c69.12 0 136.95-23.78 190.95-66.98L480 98.58v282.84z"></path></svg>
+                        </div>
+                        <div className='flex justify-center text-center items-center text-white w-full'>
+                            <h1>Digital Marketing</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className='mt-2 '>
+                    <a className='px-4 py-2 bg-[#ea4d2d] text-white hover:bg-[#2d2fbb]' href="#">
+                        View All Services
+                    </a>
+                </div>
+            </div>
+            {/* Testimonies */}
+            <div className='bg-[#2d2fbb] flex flex-col justify-center items-center pb-8'>
+                <h1 className='text-4xl text-center text-white font-bold pb-8 pt-8'>
+                    What Users Saying
+                </h1>
+                {/* 4 Services */}
+                <Carousel
+                    showArrows={true}
+                    infiniteLoop={true}
+                    showThumbs={false}
+                    showStatus={false}
+                    autoPlay={true}
+                    interval={6100}
+                >
+                    <div className='text-white flex justify-center'>
+                        <div className="w-1/2">
+                            <p className='bg-[#001b34] p-8'>
+                            <svg class="h-4" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="quote-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"></path></svg>
+                            Really satisfied with service. No need to tell them what to change they update everything time to time.
+                                <svg class="h-4" aria-hidden="true" focusable="false" data-prefix="fa" data-icon="quote-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M464 32H336c-26.5 0-48 21.5-48 48v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48zm-288 0H48C21.5 32 0 53.5 0 80v128c0 26.5 21.5 48 48 48h80v64c0 35.3-28.7 64-64 64h-8c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24h8c88.4 0 160-71.6 160-160V80c0-26.5-21.5-48-48-48z"></path></svg>
+                            </p>
+                            <img className=' h-20 w-20 object-scale-down mt-2' src="honda.png" alt="honda" />
+                            <h3 className='pt-2 text-2xl'>Modern Honda</h3>
+                            <h4 className='mb-8'>Abohar</h4>
+                        </div>
+                    </div>
+                   
+
+
+                </Carousel>
+
             </div>
         </>
     )
